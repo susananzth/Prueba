60417f8a07f9c63711f53c8da904ca4ad9e89fd5
susananzth <susananzth.dev@gmail.com>
2026-03-25 14:52:26 -0500

    feat: agregando estilo de página de inicio con la gestión de los empleado

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('employees');
+});
+
+Route::get('/employees-view', function () {
+    return view('employees');
 });
